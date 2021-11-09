b9716c485b57646646dd19f36cd3548876bca674
gorankn89 <gorankn89@gmail.com>
2021-11-09 20:00:40 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  let animalList = [
+    {vrsta:"Mrav", ime:"Atom", datumRodjenja: new Date(1543, 4, 22).toString()},
+    {vrsta:"Lav", ime:"Alex", datumRodjenja: new Date(1543, 4, 21).toString()},
+    {vrsta:"Pingvin", ime:"Vojnik", datumRodjenja: new Date(1543, 4, 20).toString()},
+    {vrsta:"Sundjer", ime:"Bob", datumRodjenja: new Date(1543, 4, 19).toString()},
+    {vrsta:"Veverica", ime:"Sendi", datumRodjenja: new Date(1543, 4, 15).toString()},
+  ]
+
+  
+let nesto = "Neki string u vezi zivotinja sve jedno";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <table>
+        <tr>
+         <th>Vrsta</th>
+         <th>Ime</th>
+         <th>Datum Rodjenja</th>
+         </tr>
+      </table>
+     {animalList.map((el) => <table><tr><td>{el.vrsta}</td><td>{el.ime}</td><td>{el.datumRodjenja}</td></tr></table>)}
+      {nesto} <br/>
+      
+
     </div>
   );
 }
